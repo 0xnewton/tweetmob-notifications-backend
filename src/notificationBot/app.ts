4d6e208fd7b8926a5cd3071e19ef6ffc36ad5248
0xnewton <0xnewton@protonmail.com>
2025-01-31 23:15:10 -0500

    update fn instances

--- src/notificationBot/app.ts
+++ src/notificationBot/app.ts
@@ -3,9 +3,12 @@ import { tgBotAPIKey } from "../lib/secrets";
 import { initializeBot } from "./bot";
 
 // Set up webhook for Telegram bot
-export const app = onRequest({ secrets: [tgBotAPIKey] }, (req, res) => {
-  const botAPIKey = tgBotAPIKey.value();
-  const bot = initializeBot(botAPIKey);
-  bot.handleUpdate(req.body);
-  res.sendStatus(200);
-});
+export const app = onRequest(
+  { secrets: [tgBotAPIKey], minInstances: 1 },
+  (req, res) => {
+    const botAPIKey = tgBotAPIKey.value();
+    const bot = initializeBot(botAPIKey);
+    bot.handleUpdate(req.body);
+    res.sendStatus(200);
+  }
+);
